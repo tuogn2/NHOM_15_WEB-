9a72b6dc452203119f428c3ed86609aa7183b233
tuogn2 <95692889+tuogn2@users.noreply.github.com>
2023-02-25 02:06:07 +0700

    edit search

---
+++ WEB/search.js
@@ -0,0 +1,22 @@
+var ble=true;
+const handleclicksearch = () => {
+    const event = document.getElementsByClassName('event');
+
+   if(ble){
+
+       event[0].innerHTML = `<div class="search"> 
+           <div class="fas fa-search search-icon"  id = "search-btn" > </div >
+           <span class="search-line"></span>
+           <input type="text" class="search-text" placeholder="Enter Laptop">
+       </div>`;
+       ble=false
+   }else{
+    event[0].innerHTML ='';
+    ble=true
+   }
+   
+
+
+   
+
+}
