9d7fc385d01b4303d0516c4d661ac2b7c25547d7
tuogn2 <95692889+tuogn2@users.noreply.github.com>
2023-04-02 21:33:11 +0700

    edit regign

--- WEB/JS/login.js
+++ WEB/JS/login.js
@@ -1,61 +1,56 @@
+const container = document.querySelector(".container");
+const containerbtn = document.querySelector(".container-btn");
+const password = document.querySelector(".signin");
 
-const container = document.querySelector('.container');
-const containerbtn = document.querySelector('.container-btn')
+const againpasswprd = document.querySelector(".repeapsignin");
 function handleclickLogin() {
-    containerbtn.innerHTML='';
+  containerbtn.innerHTML = "";
   container.innerHTML = `
-    <div class="login-container">
-      <form action="" class="form-login">
-        <h1 class="title-login">Login</h1>
-        <div class="form-infor">
-          <input type="text" class="login-email" placeholder="Name">
-          <input type="password" placeholder="Password" class="login-pass">
-          <div class="another-choice">
-            <div>
-              <input type="checkbox" id="remember">
-              <label for="remember">Remember me</label>
-            </div>
-            <a href="#">Forgot password</a>
-          </div>
-          <button class="btnlogin">Login</button>
-          <span>or use your account</span>
-          <div class="social-container">
-            <a href=""><i class="fa-brands fa-google"></i></a>
-            <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
-            <a href=""><i class="fa-brands fa-twitter"></i></a>
-          </div>
+  <div class="login-container">
+  <form action="" class="form-login">
+    <h1 class="title-login">Login</h1>
+    <div class="form-infor">
+      <input type="text" class="mai" placeholder="Name">
+      <input type="password" placeholder="Password" class="pass">
+      <div class="another-choice">
+        <div>
+          <input type="checkbox" id="remember">
+          <label for="remember">Remember me</label>
         </div>
-      </form>
+        <a href="#">Forgot password</a>
+      </div>
+      <button class="btnlogin">Login</button>
+      <span>or use your account</span>
+      <div class="social-container">
+        <a href=""><i class="fa-brands fa-google"></i></a>
+        <a href="https://www.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
+        <a href=""><i class="fa-brands fa-twitter"></i></a>
+      </div>
     </div>
+  </form>
+</div>
   `;
 }
 
-function handleclicksignin(){
-    containerbtn.innerHTML='';
-    container.innerHTML=`<div class="login-container">
-    <form action="" class="form-login">
-        <h1 style="margin-bottom: 10px;" class="title-login">SIGN IN</h1>
-        <div class="form-infor">
-            <input type="email" class="login-email" placeholder="email">
-            <input type="text" class="login-email" placeholder="Name">
-            <input type="password" placeholder="Enter Password" class="login-pass signin">
-            <input type="password" placeholder="repeap again" value="" class="login-pass repeapsignin">
-            <button onclick={checkpass()} class="btnlogin btnsignin">SIGN IN</button>
-           
-        </div>
-    </form>
-</div>`
-}
-const password = document.querySelector('.signin');
+var txtemail = "";
+var txtusername = "";
+var txtpass = "";
 
-const againpasswprd = document.querySelector('.repeapsignin');
+const checkpass = () => {
+  const emai = document.getElementById("mail");
+  const username = document.getElementById("#username");
 
-const checkpass=()=>{
-    if(password.value.length<6){
-        alert('pass phải lớn hơn 6')
-    }else if(againpasswprd.value.length !== password.value.length){
-        alert('mật khẩu mà nhập lại mật khẩu không trùng khớp')
-    }
-}
+  if (password.value.length < 6) {
+    alert("pass phải lớn hơn 6");
+    return;
+  } else if (againpasswprd.value.length !== password.value.length) {
+    alert("mật khẩu mà nhập lại mật khẩu không trùng khớp");
+    return;
+  }
+
+  txtemail = emai.value
+  txtusername = username.value
+  txtpass = password.value
 
 
+};
