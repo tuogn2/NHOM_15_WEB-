eb5eb0843c05771e33b89f2a1086361443f0a78d
vonguyenvu11223 <vonguyenvu25032003@gmail.com>
2023-04-05 17:34:20 +0700

    vu

--- WEB/JS/lienhe.js
+++ WEB/JS/lienhe.js
@@ -1,58 +1,22 @@
-
-var modalbtn = document.querySelector('.modal-btn');
-console.log(modalbtn);
-var nameInput = document.querySelector('.name');
-var emailInput = document.querySelector('.email');
-var messArea = document.querySelector('.mess');
-
-//clear
-modalbtn.onclick = function(e){
-  document.querySelector('.errorName').innerHTML = '';
-  document.querySelector('.errorEmail').innerHTML = '';
-  document.querySelector('.errorMess').innerHTML = '';
-  nameInput.value = '';
-  emailInput.value = '';
-  messArea.value = '';
-}
-
-
-form = document.querySelector('.myForm')
+const form = document.querySelector('form[name="myForm"]');
 
 form.addEventListener('submit', function(event) {
-  //Kiểm tra tên
+  const nameInput = document.querySelector('input[name="name"]');
+  const emailInput = document.querySelector('input[name="email"]');
+  const messArea = document.querySelector('textarea[name="mess"]');
+  
   if (nameInput.value === '') {
-    document.querySelector('.errorName').innerHTML = 'Vui lòng nhập tên của bạn';
+    alert('Vui lòng nhập tên của bạn');
     event.preventDefault();
   }
-  else{
-    document.querySelector('.errorName').innerHTML = '';
+  
+  if (emailInput.value === '' || !emailInput.value.includes('@')) {
+    alert('Vui lòng nhập địa chỉ email hợp lệ');
     event.preventDefault();
   }
-  //Kiểm tra mail
-  if(emailInput.value === ''){
-    document.querySelector('.errorEmail').innerHTML = 'Vui lòng nhập Mail của bạn';
-    event.preventDefault();
-  }
-  else if ( !emailInput.value.includes('@')) {
-    document.querySelector('.errorEmail').innerHTML = '';
-    event.preventDefault();
-  }
-  else{
-    document.querySelector('.errorEmail').innerHTML = '';
-    event.preventDefault();
-  }
-  //Kiểm tra mess
+
   if (messArea.value === '') {
-    document.querySelector('.errorMess').innerHTML = 'Vui lòng nhập nội dung';
-    event.preventDefault();
-  }
-  else{
-    document.querySelector('.errorMess').innerHTML = '';
+    alert('Vui lòng nhập nội dung');
     event.preventDefault();
   }
 });
-
-
-
-
-
